9b2a830365005aa76f8dcfb99160824f4a300a0d
yash bhidawe <ybh5179@gmail.com>
2025-05-24 12:28:16 +0530

    test: speech to text

--- client/src/components/Dictaphone.jsx
+++ client/src/components/Dictaphone.jsx
@@ -1,30 +1,33 @@
 import React from "react";
-import SpeechRecognition, {
-  useSpeechRecognition,
-} from "react-speech-recognition";
+import useSpeechToText from "react-hook-speech-to-text";
 
-const Dictaphone = () => {
+export default function Dictaphone() {
   const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition,
-  } = useSpeechRecognition();
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false,
+  });
 
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
-  }
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
 
   return (
     <div>
-      <p>Microphone: {listening ? "on" : "off"}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      {console.log("transcript", transcript)}
-      <p>{transcript}</p>
+      <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? "Stop Recording" : "Start Recording"}
+      </button>
+      <ul>
+        {results.map((result) => (
+          <li key={result.timestamp}>{result.transcript}</li>
+        ))}
+        {interimResult && <li>{interimResult}</li>}
+      </ul>
     </div>
   );
-};
-export default Dictaphone;
+}
